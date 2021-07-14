cf329fddcc2f3ecea4e8496743c6a785708afce0
TubaMirabilis <bkingsleygreen@gmail.com>
2021-07-14 10:38:02 +0100

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Benjamin Green on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
